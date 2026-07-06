72c9d58aa53ffdacee85e8f63c0b637b753fe5ce
unknown <marinbarber22@gmail.com>
2026-07-06 14:42:53 -0400

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,134 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /* "Exercicio 1: Faça um programa que peça ao usuário para digitar um número inteiro e informe se o número é positivo ou negativo.
+          int numero = 0;
+        Console.WriteLine("Escreva um numero inteiro: ");
+        numero = int.Parse(Console.ReadLine());
+        if (numero >= 0)
+        {
+            Console.WriteLine("O número é positivo.");
+        }
+        else
+        {
+            Console.WriteLine("O número é negativo.");
+            { }
+        }*/
+        /* Exercicio 2: Leia a idade de uma pessoa e informe se ela é maior ou menor de idade.
+        int idade = 0;
+        Console.WriteLine("Escreva a idade: ");
+        idade = int.Parse(Console.ReadLine());
+        if (idade >= 18)
+        {
+            Console.WriteLine("A pessoa é maior de idade.");
+        }
+        else
+        {
+            Console.WriteLine("A pessoa é menor de idade.");
+        }*/
+        /* Exercicio 3: Faça um programa que peça ao usuário para digitar um número inteiro e informe se o número é par ou ímpar.
+        int numero = 0;
+        Console.WriteLine("Escreva um número inteiro: ");
+        numero = int.Parse(Console.ReadLine());
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine("O número é par.");
+        }
+        else
+        {
+            Console.WriteLine("O número é ímpar.");
+        }*/
+        /* Exercicio 4: Leia a nota de um aluno e informe se ele foi aprovado ou reprovado. A nota mínima para aprovação é 7.
+        double nota = 0;
+        Console.WriteLine("Escreva a nota do aluno: ");
+        nota = double.Parse(Console.ReadLine());
+        if (nota >= 7)
+        {
+            Console.WriteLine("O aluno foi aprovado.");
+        }
+        else
+        {
+            Console.WriteLine("O aluno foi reprovado.");
+        }*/
+        /* Exercicio 5 : Peça ao usuario dois numeros e escreva o maior deles.
+        int numero1 = 0;
+        int numero2 = 0;
+        Console.WriteLine("Escreva o primeiro número: ");
+        numero1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Escreva o segundo número: ");
+        numero2 = int.Parse(Console.ReadLine());
+        if (numero1 > numero2)
+        {
+            Console.WriteLine("O primeiro número é maior.");
+        }
+        else if (numero2 > numero1)
+        {
+            Console.WriteLine("O segundo número é maior.");
+        }
+        else
+        {
+            Console.WriteLine("Os números são iguais.");
+        }*/
+        /* Exercicio 6: Leia um salario e se ele for menor que 1500,00 escreva "Salário abaixo da media", caso contrário escreva "Salário acima da media".
+        double salario = 0;
+        Console.WriteLine("Escreva o salário: ");
+        salario = double.Parse(Console.ReadLine());
+        if (salario < 1500)
+        {
+            Console.WriteLine("Salário abaixo da média.");
+        }
+        else
+        {
+            Console.WriteLine("Salário acima da media.");
+        }*/
+        /* Exercicio 7: Leia uma temperatura e se ela for maior que 30 graus escreva "Está calor"
+        double temperatura = 0;
+        Console.WriteLine("Escreva a temperatura: ");
+        temperatura = double.Parse(Console.ReadLine());
+        if (temperatura > 30)
+        {
+            Console.WriteLine("Está calor.");
+        }
+        else
+        {
+            Console.WriteLine("Não está calor.");
+        }*/
+        /* Exercicio 8: peça ao usuario para digitar um numero e se for divisivel por 5 escreva "O número é divisível por 5", caso contrário escreva "O número não é divisível por 5".
+        int numero = 0;
+        Console.WriteLine("Escreva um número: ");
+        numero = int.Parse(Console.ReadLine());
+        if (numero % 5 == 0)
+        {
+            Console.WriteLine("O número é divisível por 5.");
+        }
+        else
+        {
+            Console.WriteLine("O número não é divisível por 5.");
+        }*/
+        /* Exercicio 9: Leia a quantidade de produtos de um estoque se for menor que 10 escreva "Estoque baixo", caso contrário escreva "Estoque normal".
+        int quantidade = 0;
+        Console.WriteLine("Escreva a quantidade de produtos no estoque: ");
+        quantidade = int.Parse(Console.ReadLine());
+        if (quantidade < 10)
+        {
+            Console.WriteLine("Estoque baixo.");
+        }
+        else
+        {
+            Console.WriteLine("Estoque normal.");
+        }*/
+        /* Exercicio 10: Leia um carcter e se for igual a 's" exiba "confirmado"
+        char caracter = ' ';
+        Console.WriteLine("Escreva um caractere: ");
+        caracter = char.Parse(Console.ReadLine());
+        if (caracter == 's')
+        {
+            Console.WriteLine("Confirmado.");
+        }
+        else
+        {
+            Console.WriteLine("Não confirmado.");
+        }*/
+    }
+}
